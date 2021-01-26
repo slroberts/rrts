2a8b053576e69540de73701d43ecc4c87f1ee8c7
Shomari Roberts <shomariroberts@gmail.com>
2021-01-26 09:26:16 -0500

    broke out action creators and centralized imports

---
+++ src/actions/todos.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+// Model of the structure that an action should have
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    // FetchTodosAction makes sure I am always passing
+    // in an object with the correct types and properties
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id,
+  };
+};

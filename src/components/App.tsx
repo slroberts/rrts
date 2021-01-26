1f75ae90d3b6ccc67129a17ae4c286198e29bc70
Shomari Roberts <shomariroberts@gmail.com>
2021-01-26 09:17:30 -0500

    rendered list of todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,10 +13,17 @@ class _App extends React.Component<AppProps> {
     this.props.fetchTodos();
   };
 
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
       </div>
     );
   }
